391537bc987eed8f26ee25131355fdd3003b7737
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-11-06 16:25:10 +0100

    ipdate styles

--- public/font/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Montserrat } from 'next/font/google'
-
-export const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['700'], // you can add more like ['400', '500', '700']
-  preload  : true,
-  display : "swap"
-})
-
